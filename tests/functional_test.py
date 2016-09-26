187590caabc067b1bbd576148e6d184f26eb1401
Fabio Fortkamp <fabio@fabiofortkamp.com>
2016-09-26 13:37:42 -0300

    Added very basic functional tests file, with a simple story and a reminder to finish tests

---
+++ tests/functional_test.py
@@ -0,0 +1,7 @@
+# I want to see how the teslamax script works. Following standard Unix rules,
+# I run 'teslamax --help' from an arbitrary directory and get a helpful message
+
+# finish writing the tests!
+def test_finish_tests():
+    assert 0, "Finish the functional tests!"
+
