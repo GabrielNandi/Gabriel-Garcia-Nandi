8c2e64065f57e8863fea0ae25bd4419fdcb9e459
Fabio Fortkamp <fabio@fabiofortkamp.com>
2016-09-26 13:55:20 -0300

    Expanded functional tests with auxiliary functions to run the script and a test to run with a help flag

--- tests/functional_test.py
+++ tests/functional_test.py
@@ -1,5 +1,52 @@
+import subprocess
+
+HELP_FLAG = '--help'
+
+HELP_MESSAGE = """Usage: teslamax [--help]
+
+Read 'comsol-parameters.txt' from the current directory and execute the
+TeslaMax COMSOL model
+
+Options:
+
+-h, --help    Print this help message and exit
+"""
+
+TESLAMAX_COMMAND = 'teslamax'
+
+def get_output_from_teslamax_command(work_dir,flag=None):
+    """Run the teslamax command from 'work_dir', possibly
+    passing 'flag', and return a tuple (status_code, output_message)
+    """
+
+    teslamax_args = [TESLAMAX_COMMAND]
+
+    if flag:
+        teslamax_args.append(flag)
+
+    process = subprocess.run(teslamax_args,
+                             stdout=subprocess.PIPE,
+                             stderr=subprocess.STDOUT,
+                             universal_newlines=True,
+                             cwd=work_dir,
+                             shell=True)
+
+    return (process.returncode, process.stdout)
+
+    
+
+
 # I want to see how the teslamax script works. Following standard Unix rules,
 # I run 'teslamax --help' from an arbitrary directory and get a helpful message
+def test_run_command_with_help_flag_return_message(tmpdir):
+
+    work_dir = tmpdir.strpath
+
+    status_code, return_message = get_output_from_teslamax_command(work_dir,
+                                                                   HELP_FLAG)
+
+    assert return_message == HELP_MESSAGE
+
 
 # finish writing the tests!
 def test_finish_tests():
