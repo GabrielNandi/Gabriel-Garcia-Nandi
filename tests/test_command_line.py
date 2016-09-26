9455d7fd0f4ada91ad026af5dc21a5db85573af0
Fabio Fortkamp <fabio@fabiofortkamp.com>
2016-09-26 14:24:11 -0300

    Modified command line unit test to print a helpful message

--- tests/test_command_line.py
+++ tests/test_command_line.py
@@ -5,4 +5,8 @@ TESLAMAX_COMMAND = 'teslamax'
 # check to see if the 'teslamax' command is in the PATH
 def test_teslamax_in_system_path():
 
-    assert shutil.which(TESLAMAX_COMMAND)
+    install_message = "Make sure to install teslamax by running " + \
+                      "'pip install .' from the main directory"
+
+    assert shutil.which(TESLAMAX_COMMAND), install_message
+    
