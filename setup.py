b36cf6f67831457536b2ff97bbe28ecd4918429d
Fabio Fortkamp <fabio@fabiofortkamp.com>
2016-09-26 14:26:48 -0300

    Created basic package structure and setup script

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+package = 'teslamax'
+version = '0.1'
+
+setup(name=package,
+      version=version,
+      description="A script to run the TeslaMax COMSOL model from the command line",
+      packages=['teslamax'],
+      entry_points={
+          'console_scripts': [
+              'teslamax = teslamax.__main__:main'
+              ]
+          },
+      license=open('LICENSE').read(),
+      author="Fábio Fortkamp")
