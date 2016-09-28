2b69c771a4466c9fe472d381ee7cbd4dfc2db246
Fabio Fortkamp <fabio@fabiofortkamp.com>
2016-09-28 11:03:35 -0300

    Create test about simply calling the script

--- tests/functional_tests/test_functional.py
+++ tests/functional_tests/test_functional.py
@@ -15,6 +15,25 @@ def test_run_command_with_help_flag_return_message(tmpdir):
     assert return_message == conftest.HELP_MESSAGE
 
 
+# I then create a correct parameters file and call the script with no arguments
+# The teslamax program name will execute the Java class file and
+# return sucessfully
+def test_run_command_with_no_flag_return_success(tmpdir):
+
+    # based on some experimentation, a string "Total time: ..." is the last
+    # string to be printed if the COMSOL model is ran successfully
+
+    work_dir = tmpdir.strpath
+    
+    status_code, return_message = conftest.get_output_from_teslamax_command(work_dir)
+
+    success_string = "Total time: "
+
+    # make sure that the success string is contained in the last line
+    # (hence the split and the -1 index) of the return message
+    assert success_string in return_message.split()[-1]
+    
+
 # finish writing the tests!
 def test_finish_tests():
     assert 0, "Finish the functional tests!"
