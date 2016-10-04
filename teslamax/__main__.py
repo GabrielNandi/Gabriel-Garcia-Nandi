166003a7f1619dca4e82c45093dd757f6a56d57c
Fabio Fortkamp <fabio@fabiofortkamp.com>
2016-10-04 08:52:55 -0300

    Created cheating function to write an empty results file

--- teslamax/__main__.py
+++ teslamax/__main__.py
@@ -11,7 +11,15 @@ Options:
 import subprocess
 import os.path
 import sys
+from pathlib import Path
 from docopt import docopt
+
+B_HIGH_FILENAME = "B_high.txt"
+B_LOW_FILENAME = "B_low.txt"
+
+MAIN_RESULTS_FILENAME = "COMSOL Main Results.txt"
+
+MAGNETIC_PROFILE_FILENAME = "COMSOL Magnetic Profile.txt"
 
 def get_teslamax_class_file_path():
     """Return the path string to the main TeslaMax java class file.
@@ -26,6 +34,17 @@ def get_teslamax_class_file_path():
                             "java")
     class_file = os.path.join(java_dir,"TeslaMax.class")
     return os.path.realpath(class_file)
+
+def write_main_results_file():
+    """Create a file "COMSOL Main Results.txt" in the current directory,
+    assuming the teslamax command was already ran
+    
+    """
+
+    p = Path(MAIN_RESULTS_FILENAME)
+    p.touch()
+    
+    
 
 def run_comsol_mode():
     """Run the TeslaMax COMSOL Java model from the current directory
@@ -45,6 +64,10 @@ def run_comsol_mode():
                                         universal_newlines=True)
 
     print(comsol_cmd_process.stdout,end='')
+
+    write_main_results_file()
+    write_magnetic_profile_file()
+    
     sys.exit(comsol_cmd_process.returncode)
 
 def main():
