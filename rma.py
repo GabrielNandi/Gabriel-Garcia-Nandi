4c445f4940bc26e9e0367261b03c1438b60ecbd0
Fábio Fortkamp <fabio@fabiofortkamp.com>
2019-03-07 12:42:51 -0300

    Created pseudocode script to run mesh analysis

---
+++ rma.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+rma.py - Run Mesh Analysis
+
+This script runs the TeslaMax model with various mesh configurations
+and plot the results for high field and computing time.
+
+"""
+
+# Define constant parameters for the TeslaMax model
+
+# Define variable parameters for the TeslaMax model
+
+# Define the default mesh parameters to test
+# and a range of mesh parameters around it
+
+# Define where to save the simulation files
+
+# For each mesh configuration, and for each geometry:
+#  run the model,
+#  calculate the maximum magnetic field,
+#  while computing the time required for that computation
+#  and the number of mesh elements
+
+# Create plots of B and t as function of number of elements,
+# with curves for each geometry
